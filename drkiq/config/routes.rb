58dd5463bc84b602299a44ad9375f691533845ff
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-10 16:44:01 +0700

    Rollback post resource

--- drkiq/config/routes.rb
+++ drkiq/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts
   get 'pages', to: 'pages#index'
 
   root 'pages#home'
--- drkiq/db/schema.rb
+++ drkiq/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_209_181_939) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_093041) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'articles', force: :cascade do |t|
-    t.integer 'type'
-    t.string 'title'
-    t.text 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
